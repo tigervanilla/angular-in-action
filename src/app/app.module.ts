177e65bc85ff75a022ee3c0bcd6ac81b84787f4f
piyushmaheshwari <piyush.maheshwari@geminisolutions.in>
2020-01-09 15:47:54 +0530

    helpful comments in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+// import angular dependencies needed
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// import the app component
 import { AppComponent } from './app.component';
 
+// NgModule annotation to define a module by passing an object
 @NgModule({
-  declarations: [
+  declarations: [ // declarations are to list any components and directives used in the app
     AppComponent
   ],
-  imports: [
+  imports: [ // other modules that are used in the app
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // Providers are any services used in the app
+  bootstrap: [AppComponent] // Bootstrap declares which component to use as the first to bootstrap the application
 })
 export class AppModule { }
